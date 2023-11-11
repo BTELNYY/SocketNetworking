5901f62c57c06bd499f0ed0934cef14fbca03ca4
BTELNYY <btelnyy@gmail.com>
2023-11-10 19:52:09 -0600

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # SocketNetworking
- A Networking Library using TCP and packets (am bored)
+ A Networking Library using TCP/IP and custom packets.
+
+ The main parts you'll be using are NetworkClient, NetworkServer, INetworkObject, CustomPacket, and NetworkManager.
+
+ ## NetworkClient
+  The main Class for handling all communication. 
+
+  * If you are on the Local side (Client), You will create an instance of this class and Call the `InitLocalClient()` method. After this, you'll need to call the `Connect()` method.
+
+  * If you are on the server, you do not create the NetworkClient instance, the library will for you.
+
+ ## NetworkServer
+ Class for handling all Server related stuff.
+
+  * You can specify the Bind IP, Listen Port, or the Type the NetworkClient should have.
+
+  * When you are ready, call `StartServer()` NOTE: You should set the variables you wish to modify BEFORE starting the server.
--- SocketNetworking/NetworkClient.cs
+++ SocketNetworking/NetworkClient.cs
@@ -416,6 +416,19 @@ namespace SocketNetworking
         }
 
 
+        private static byte[] ShiftOut(ref byte[] input, int count)
+        {
+            byte[] output = new byte[count];
+            // copy the first N elements to output
+            Buffer.BlockCopy(input, 0, output, 0, count);
+            // copy the back of the array forward (removing the elements we copied to output)
+            // ie with count 2: [1, 2, 3, 4] -> [3, 4, 3, 4]
+            // we dont update the end of the array to fill with zeros, because we dont need to (we just call it undefined behavior)
+            Buffer.BlockCopy(input, count, input, 0, count);
+
+            return output;
+        }
+
         /// <summary>
         /// Only called on the server.
         /// </summary>
@@ -487,20 +500,6 @@ namespace SocketNetworking
                     break;
             }
         }
-
-
-        private static byte[] ShiftOut(ref byte[] input, int count)
-        {
-            byte[] output = new byte[count];
-            // copy the first N elements to output
-            Buffer.BlockCopy(input, 0, output, 0, count); 
-            // copy the back of the array forward (removing the elements we copied to output)
-            // ie with count 2: [1, 2, 3, 4] -> [3, 4, 3, 4]
-            // we dont update the end of the array to fill with zeros, because we dont need to (we just call it undefined behavior)
-            Buffer.BlockCopy(input, count, input, 0, count); 
-            
-            return output;
-        }  
         
         /// <summary>
         /// Only called on the client.
