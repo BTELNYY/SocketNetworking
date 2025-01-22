8bdcab08837ed831dbecaa49b76d4c82f1803bbc
BTELNYY <btelnyy@gmail.com>
2025-01-22 15:25:14 -0600

    More tests

--- SocketNetworking.Tests/EncryptionTest.cs
+++ SocketNetworking.Tests/EncryptionTest.cs
@@ -24,5 +24,20 @@ namespace SocketNetworking.Tests
             byte[] encrypted = manager.Encrypt(bytes);
             Assert.IsTrue(Enumerable.SequenceEqual(old, manager.Decrypt(encrypted)));
         }
+
+        [TestMethod]
+        public void TestAsym()
+        {
+            Random random = new Random();
+            byte[] bytes = new byte[128];
+            random.NextBytes(bytes);
+            byte[] old = new byte[bytes.Length];
+            bytes.CopyTo(old, 0);
+            Assert.IsTrue(Enumerable.SequenceEqual(old, bytes));
+            NetworkEncryptionManager manager = new NetworkEncryptionManager();
+            byte[] encrypted = manager.Encrypt(bytes, false, true);
+            byte[] returned = manager.Decrypt(encrypted, false);
+            Assert.IsTrue(Enumerable.SequenceEqual(old, returned));
+        }
     }
 }
